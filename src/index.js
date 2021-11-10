ab1d84f29659e40dc466721a72cf2c3021d85379
Viktoriia Huk <83830229+Huk2021@users.noreply.github.com>
2021-11-10 21:50:19 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ function infoCountry([{ name, flags, capital, population, languages }]) {
        <p class = "info"> Capital: <span class = "info-span">${capital}</span></p>
        <p class = "info"> Population: <span class = "info-span">${population}</span></p>
        <p class = "info"> Languages: <span class = "info-span">${Object.values(languages)
-        .join('')}
+        .join(',')}
         </span></p>`;
    
  }
